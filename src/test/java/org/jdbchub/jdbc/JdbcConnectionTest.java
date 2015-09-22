d909cdd9982ba9d2a17f38ea2f0f3fe7ba237e0f
daring <daring@spb.ant-inform.ru>
2015-09-22 19:18:43 +0300

    extend JdbcConnectionTest

--- src/test/java/org/jdbchub/jdbc/JdbcConnectionTest.java
+++ src/test/java/org/jdbchub/jdbc/JdbcConnectionTest.java
@@ -3,13 +3,11 @@ package org.jdbchub.jdbc;
 import org.junit.Test;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
-
 import java.sql.Connection;
 import java.util.List;
-
 import static org.jdbchub.JdbcHubTestUtils.allItems;
 import static org.jdbchub.JdbcHubTestUtils.createTestDatabases;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class JdbcConnectionTest {
 
@@ -19,6 +17,22 @@ public class JdbcConnectionTest {
 		JdbcConnection c = new JdbcConnection(cons);
 		try (Handle h = DBI.open(c)) {
 			assertEquals(allItems(), h.select("select * from test_items"));
+		}
+	}
+
+	@Test
+	public void testClose() throws Exception {
+		List<Connection> cons = createTestDatabases();
+		try (JdbcConnection c = new JdbcConnection(cons)) {
+			assertFalse(c.isClosed());
+
+			cons.get(0).close();
+			assertTrue(c.isClosed());
+			assertFalse(c.map(Connection::isClosed).allMatch(r -> r));
+
+			c.close();
+			assertTrue(c.isClosed());
+			assertTrue(c.map(Connection::isClosed).allMatch(r -> r));
 		}
 	}
 
