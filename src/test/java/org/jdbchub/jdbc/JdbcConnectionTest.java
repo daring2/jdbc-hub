5d211d01828c1877645e41be54342517ac4de02a
daring <daring@spb.ant-inform.ru>
2015-09-27 11:53:41 +0300

    JdbcConnectionTest - add savepoint test

--- src/test/java/org/jdbchub/jdbc/JdbcConnectionTest.java
+++ src/test/java/org/jdbchub/jdbc/JdbcConnectionTest.java
@@ -3,9 +3,14 @@ package org.jdbchub.jdbc;
 import org.junit.Test;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
+import org.skife.jdbi.v2.Query;
+import org.skife.jdbi.v2.Update;
+import org.skife.jdbi.v2.util.StringMapper;
 
 import java.sql.Connection;
+import java.sql.Savepoint;
 
+import static java.util.Arrays.asList;
 import static org.jdbchub.JdbcHubTestUtils.allItems;
 import static org.jdbchub.JdbcHubTestUtils.createTestConnection;
 import static org.junit.Assert.*;
@@ -41,6 +46,23 @@ public class JdbcConnectionTest {
 			assertTrue(c.isValid(1));
 			c.entity(0).close();
 			assertFalse(c.isValid(1));
+		}
+	}
+
+	@Test
+	public void testSavepoint() throws Exception {
+		try (JdbcConnection c = createTestConnection()) {
+			Handle h = DBI.open(c);
+			Update st = h.createStatement("update test_items set value = ? where name like '%1'");
+			Query<String> q = h.createQuery("select value from test_items where name like '%1'").map(StringMapper.FIRST);
+			h.begin();
+			st.bind(0, "nv1").execute();
+			assertEquals(asList("nv1", "nv1", "nv1"), q.list());
+			Savepoint sp = c.setSavepoint();
+			st.bind(0, "nv2").execute();
+			assertEquals(asList("nv2", "nv2", "nv2"), q.list());
+			c.rollback(sp);
+			assertEquals(asList("nv1", "nv1", "nv1"), q.list());
 		}
 	}
 
