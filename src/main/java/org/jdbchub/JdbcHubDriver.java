90aabd1576bc4ca35997d6a06281a7d905582da5
Danila Valikov <danila.daring@gmail.com>
2015-09-30 23:01:25 +0300

    refactoring, reformat

--- src/main/java/org/jdbchub/JdbcHubDriver.java
+++ src/main/java/org/jdbchub/JdbcHubDriver.java
@@ -6,41 +6,44 @@ import java.util.logging.Logger;
 
 public class JdbcHubDriver implements Driver {
 
-    public static final String UrlPrefix = "jdbc:jdbchub:";
+	public static final String UrlPrefix = "jdbc:jdbchub:";
 
-    @Override
-    public boolean acceptsURL(String url) throws SQLException {
-        return url != null && url.startsWith(UrlPrefix);
-    }
+	@Override
+	public boolean acceptsURL(String url) throws SQLException {
+		return url != null && url.startsWith(UrlPrefix);
+	}
 
-    @Override
-    public Connection connect(String url, Properties info) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public Connection connect(String url, Properties info) throws SQLException {
+		if (!acceptsURL(url))
+			return null;
+		String configFile = url.substring(UrlPrefix.length());
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
-        return new DriverPropertyInfo[0];
-    }
+	@Override
+	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+		return new DriverPropertyInfo[0];
+	}
 
-    @Override
-    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-        return Logger.getLogger("org.jdbchub");
-    }
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		return Logger.getLogger("org.jdbchub");
+	}
 
-    @Override
-    public int getMajorVersion() {
-        return 1;
-    }
+	@Override
+	public int getMajorVersion() {
+		return 1;
+	}
 
-    @Override
-    public int getMinorVersion() {
-        return 0;
-    }
+	@Override
+	public int getMinorVersion() {
+		return 0;
+	}
 
-    @Override
-    public boolean jdbcCompliant() {
-        return false;
-    }
+	@Override
+	public boolean jdbcCompliant() {
+		return false;
+	}
 
 }
