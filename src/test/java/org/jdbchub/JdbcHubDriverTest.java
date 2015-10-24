0255fff233aa4e1f7e774f7068ddf9e8c7aea651
Danila Valikov <danila.daring@gmail.com>
2015-10-24 21:17:43 +0300

    extend JdbcHubDriverTest

--- src/test/java/org/jdbchub/JdbcHubDriverTest.java
+++ src/test/java/org/jdbchub/JdbcHubDriverTest.java
@@ -1,11 +1,14 @@
 package org.jdbchub;
 
+import org.jdbchub.jdbc.HubConnection;
 import org.junit.Test;
-
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+import java.sql.Connection;
 import java.sql.SQLException;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static java.util.Arrays.asList;
+import static org.jdbchub.JdbcHubTestUtils.*;
+import static org.junit.Assert.*;
 
 public class JdbcHubDriverTest {
 
@@ -17,8 +20,15 @@ public class JdbcHubDriverTest {
 	}
 
 	@Test
-	public void testConnect() {
-		// TODO implement
+	public void testConnect() throws SQLException {
+		JdbcHubDriver driver = new JdbcHubDriver();
+		String url = "jdbc:jdbchub:c1.conf";
+		try (HubConnection hc = createTestConnection(); Connection c = driver.connect(url, null)) {
+			Handle h = DBI.open(c);
+			assertEquals(allItems(), h.select("select * from test_items"));
+			assertEquals(asList("db1", "db2", "db3"), selectStringList(h, "select '${db_name}'"));
+
+		}
 	}
 
 }
