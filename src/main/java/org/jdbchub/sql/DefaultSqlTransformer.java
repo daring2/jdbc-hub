b958913c13f143cf769cf9c137cf075c848841d8
Danila Valikov <danila.daring@gmail.com>
2015-10-25 00:22:39 +0300

    DefaultSqlTransformer - use StrSubstitutor

--- src/main/java/org/jdbchub/sql/DefaultSqlTransformer.java
+++ src/main/java/org/jdbchub/sql/DefaultSqlTransformer.java
@@ -1,19 +1,29 @@
 package org.jdbchub.sql;
 
+import org.apache.commons.lang3.text.StrSubstitutor;
 import org.jdbchub.config.DBConfig;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DefaultSqlTransformer implements SqlTransformer {
 
 	private final DBConfig dbConfig;
+	private final StrSubstitutor substitutor;
 
 	public DefaultSqlTransformer(DBConfig dbConfig) {
 		this.dbConfig = dbConfig;
+		this.substitutor = buildSubstitutor();
+	}
+
+	private StrSubstitutor buildSubstitutor() {
+		Map<String, String> vars = new HashMap<>();
+		vars.put("db_name", dbConfig.name);
+		return new StrSubstitutor(vars);
 	}
 
 	@Override
 	public String transform(String sql) {
-		//TODO optimize
-		return sql.replace("${db_name}", dbConfig.name);
+		return substitutor.replace(sql);
 	}
 
 }
