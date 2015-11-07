ae83613f70ccf097c4ab00c618dcf6698533c854
Danila Valikov <danila.daring@gmail.com>
2015-11-08 01:18:58 +0300

    extend RegexSqlTransformerTest

--- src/test/java/org/jdbchub/sql/RegexSqlTransformerTest.java
+++ src/test/java/org/jdbchub/sql/RegexSqlTransformerTest.java
@@ -12,6 +12,7 @@ public class RegexSqlTransformerTest {
 		Config c = configFromString("rules { \"k1\" = \"v1\", \"k2(.)\" = \"v2$1\" } ");
 		RegexSqlTransformer tr = new RegexSqlTransformer(c);
 		assertEquals(2, tr.rules.size());
+		assertEquals("k0", tr.transform("k0"));
 		assertEquals("k0 v1 v21 v22 k31", tr.transform("k0 k1 k21 k22 k31"));
 	}
 
