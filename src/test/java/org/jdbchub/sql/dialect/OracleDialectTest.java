64d2dca0783086e990014e872fde2ec8b9c24c34
Danila Valikov <danila.daring@gmail.com>
2015-11-14 17:10:42 +0300

    oracle-dialect.conf - add mssqlToOracle transformer

--- src/test/java/org/jdbchub/sql/dialect/OracleDialectTest.java
+++ src/test/java/org/jdbchub/sql/dialect/OracleDialectTest.java
@@ -10,20 +10,28 @@ import static org.junit.Assert.*;
 
 public class OracleDialectTest {
 
-	Config config = configFromResource("oracle-dialect.conf").getConfig("anyToOracle");
+	Config config = configFromResource("oracle-dialect.conf");
 
 	@Test
-	public void testIsEnabled() {
-		SqlTransformer tr = new RegexSqlTransformer(config);
+	public void testAnyToOracle() {
+		SqlTransformer tr = new RegexSqlTransformer(config.getConfig("anyToOracle"));
+
 		assertTrue(tr.isEnabled(dbConfig("db1", "jdbc:h2:mem:db1")));
 		assertFalse(tr.isEnabled(dbConfig("db1", "jdbc:oracle:thin:localhost:db1")));
+
+		assertEquals("select 'v1'", tr.transform("select 'v1'"));
+		assertEquals("select 'v2' from (select 'X' DUMMY) dual", tr.transform("select 'v2' from dual"));
 	}
 
 	@Test
-	public void testTransform() {
-		SqlTransformer tr = new RegexSqlTransformer(config);
+	public void testMssqlToOracle() {
+		SqlTransformer tr = new RegexSqlTransformer(config.getConfig("mssqlToOracle"));
+
+		assertTrue(tr.isEnabled(dbConfig("db1", "jdbc:jtds:sqlserver://localhost:1433/db1")));
+		assertFalse(tr.isEnabled(dbConfig("db1", "jdbc:h2:mem:db1")));
+
 		assertEquals("select 'v1'", tr.transform("select 'v1'"));
-		assertEquals("select 'v2' from (select 'X' DUMMY) dual", tr.transform("select 'v2' from dual"));
+		assertEquals("select 'v1' + 'v2'", tr.transform("select 'v1'||  'v2'"));
 	}
 
 }
